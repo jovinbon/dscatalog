5c958d2f85bc97df3a024f427baa028fcbd387eb
jovinbon <alysson_cdf@hotmail.com>
2022-02-07 21:46:50 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,15 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import NavBar from 'components/NavBar';
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello DSCatalog</h1>
+    <>
+    <NavBar />
+    <h1>Hello DSCatalog</h1>;
+    </>
   );
+  
 }
 
 export default App;
