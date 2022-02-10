43cc5be84ce2af35254c9caaf8e5e4a669a8c0dd
jovinbon <alysson_cdf@hotmail.com>
2022-02-10 20:09:55 -0300

    Home styling

---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,29 @@
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
+import NavBar from 'components/NavBar';
+
+import './styles.css';
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="home-container">
+        <div className="home-card">
+            <div className="home-content-container">
+              <div>
+                  <h1>Conheça o melhor catálogo de produtos</h1>
+                  <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
+              </div>
+               <ButtonIcon />
+            </div>
+            <div className="home-image-container">
+               <MainImage />
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
