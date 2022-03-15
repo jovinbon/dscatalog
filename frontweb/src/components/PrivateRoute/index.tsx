f7a37f6abff3b79a61cc200647ba97e7a14321f0
jovinbon <jovinbon@hotmail.com>
2022-03-14 21:51:07 -0300

    Route level permission

--- frontweb/src/components/PrivateRoute/index.tsx
+++ frontweb/src/components/PrivateRoute/index.tsx
@@ -1,22 +1,33 @@
 import { Redirect, Route } from 'react-router-dom';
-import { isAuthenticated } from 'util/requests';
+import { hasAnyRole, isAuthenticated, Role } from 'util/requests';
 
 type Props = {
   children: React.ReactNode;
   path: string;
+  roles?: Role[];
 };
 
-const PrivateRoute = ({ children, path }: Props) => {
-
+const PrivateRoute = ({ children, path, roles = [] }: Props) => {
   return (
     <Route
       path={path}
-      render={({location}) =>
-        isAuthenticated() ? children : <Redirect to={{
+      render={({ location }) =>
+        !isAuthenticated() ? (
+          <Redirect
+            to={{ pathname: '/admin/auth/login', state: { from: location } }}
+          />
+        ) : !hasAnyRole(roles) ? (
+          <Redirect to="/admin/products" />
+        ) : (
+          children
+        )
+      }
+
+      /*isAuthenticated() ? children : <Redirect to={{
           pathname: "/admin/auth/login",
           state: {  from: location}
         }} />
-      }
+      }*/
     />
   );
 };
