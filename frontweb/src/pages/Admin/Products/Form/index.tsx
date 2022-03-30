5427a76a0736982adbcc223be036d0b2e96100d3
jovinbon <jovinbon@hotmail.com>
2022-03-30 20:52:22 -0300

    Float format

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
+import CurrencyInput from 'react-currency-input-field';
 import { useForm, Controller } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
@@ -50,10 +51,12 @@ const Form = () => {
 
   const onSubmit = (productData: Product) => {
 
+    const data = {...productData, price: String(productData.price).replace(",",".")};
+
     const config: AxiosRequestConfig = {
       method: isEditing ? 'PUT' : 'POST',
       url: isEditing ? `/products/${productId}` : "/products",
-      data: productData,
+      data,
       withCredentials: true
     };
 
@@ -108,14 +111,19 @@ const Form = () => {
                   }
               </div>
               <div className="margin-bottom-30">
-                  <input
-                    {...register('price', {
-                        required: 'Campo obrigatório.'
-                    })}
-                    type="text"
-                    className={`form-control base-input ${errors.price ? 'is-invalid' : ''}`}
-                    placeholder="Preço"
+                  <Controller 
                     name="price"
+                    rules={{required: 'Campo obrigatório'}}
+                    control={control}
+                    render={({field}) => (
+                      <CurrencyInput 
+                      placeholder="Preço"
+                      className={`form-control base-input ${errors.price ? 'is-invalid' : ''}`}
+                      disableGroupSeparators={true}
+                      value={field.value}
+                      onValueChange={field.onChange}
+                      />
+                    )}
                   />
                   <div className="invalid-feedback d-block">{errors.price?.message}</div>
               </div>
