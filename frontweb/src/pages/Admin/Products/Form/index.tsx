52f2f7a035fe9f103f7298fdfc3e364818533026
jovinbon <jovinbon@hotmail.com>
2022-03-30 20:10:36 -0300

    Select React Hook Form integration

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,6 +1,6 @@
 import { AxiosRequestConfig } from 'axios';
 import { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
 import { Category } from 'types/category';
@@ -22,7 +22,7 @@ const Form = () => {
 
   const history = useHistory();
 
-  const { register, handleSubmit, formState: {errors}, setValue } = useForm<Product>();
+  const { register, handleSubmit, formState: {errors}, setValue, control } = useForm<Product>();
 
   useEffect(() => {
     if (isEditing) {
@@ -90,13 +90,25 @@ const Form = () => {
                   <div className="invalid-feedback d-block">{errors.name?.message}</div>
               </div>
               <div className="margin-bottom-30">
-                  <Select 
-                    options={selectCategories}
-                    classNamePrefix="product-crud-select"
-                    isMulti
-                    getOptionLabel={(category: Category) => category.name}
-                    getOptionValue={(category: Category) => String(category.id)}
-                  />
+                  <Controller 
+                    name="categories"
+                    rules={{required: true}}
+                    control={control}
+                    render={({field}) => (
+                      <Select {...field} 
+                        options={selectCategories}
+                        classNamePrefix="product-crud-select"
+                        isMulti
+                        getOptionLabel={(category: Category) => category.name}
+                        getOptionValue={(category: Category) => String(category.id)}
+                    />
+                    )}
+                  />  
+                  {errors.categories && (
+                    <div className="invalid-feedback d-block">Campo obrigatório</div>
+                  )
+
+                  }
               </div>
               <div className="margin-bottom-30">
                   <input
